5f910e9eb5e0aa0e68129620d262d7e1def54c5a
Taylor Williams <taylor@thewilliams.net>
2018-11-19 02:27:14 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,27 +5,38 @@ class SongsController < ActionController::Base
   end
 
   def create
-    @song = Song.create(song_attributes)
+    @song = Song.new(song_attributes)
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
   end
 
   def edit
-    
+    @song = Song.find(params[:id])
   end
 
   def update
-    
+    @song = Song.find(params[:id])
+    if @song.update(song_attributes)
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
   end
 
   def destroy
-    
+    @song.find(params[:id]).destroy
+    redirect_to songs_path
   end
 
   def show
-    
+    @song = Song.find(params[:id])
   end
 
   def index
-    
+    @songs = Song.all
   end
 
   private
